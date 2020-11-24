f59cd94f8c63b60f1593900675de9181857b4174
Andrew <dawdrew@gmail.com>
2020-11-24 02:41:27 -0500

    First commit

--- WalmartBot_PS5_Digital.py
+++ WalmartBot_PS5_Digital.py
@@ -1,27 +1,34 @@
 from selenium import webdriver
 import time
 
+
 def order():
-    #VARIABLES
+    # VARIABLES
     addToCart = '//*[@id="add-on-atc-container"]/div[1]/section/div[1]/div[3]/button/span/span'
-    checkOut = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div/div[2]/div/button[1]'
-    continueWithoutAccount = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[3]/div/div[1]/div/section/section/div/button/span'
-    firstContinue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[3]/div/div/div[2]/button/span'
-    firstName ='//*[@id="firstName"]'
+    checkOut = '//*[@id="cart-root-container-content-skip"]/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[' \
+               '2]/div/div[2]/div/button[1] '
+    continueWithoutAccount = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[1]/div/div/div/div/div[' \
+                             '3]/div/div[1]/div/section/section/div/button/span '
+    firstContinue = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[2]/div/div[2]/div/div/div/div[' \
+                    '3]/div/div/div[2]/button/span '
+    firstName = '//*[@id="firstName"]'
     lastName = '//*[@id="lastName"]'
     email = '//*[@id="email"]'
     address = '//*[@id="addressLineOne"]'
     phone = '//*[@id="phone"]'
-    confirmInfo = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[3]/div/div/div/div/div/form/div[2]/div[2]/button/span'
+    confirmInfo = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]/div/div/div/div[' \
+                  '3]/div/div/div/div/div/form/div[2]/div[2]/button/span '
     creditCardNum = '//*[@id="creditCard"]'
     creditExpireMonth = '//*[@id="month-chooser"]'
     creditExpireYear = '//*[@id="year-chooser"]'
     creditCVV = '//*[@id="cvv"]'
-    reviewOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span'
-    confirmOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div/form/div/button'
+    reviewOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div/div[4]/div[1]/div[2]/div/div/div/div[' \
+                  '3]/div[2]/div/div/div/div[2]/div/div/div/form/div[3]/div/button/span/span/span '
+    confirmOrder = '/html/body/div[1]/div/div[1]/div/div[1]/div[3]/div/div/div[2]/div[1]/div[2]/div/div/div[' \
+                   '2]/div/form/div/button '
 
-    #KEYS
-    #add your information here
+    # KEYS
+    # add your information here
     myFirstName = 'John'
     myLastName = 'Smith'
     myEmail = 'mail@gmail.com'
@@ -32,12 +39,12 @@ def order():
     myCreditExpireYear = '25'
     myCVV = '123'
 
-    #ADDS PS5 TO CART AND GOES TO CHECKOUT
+    # ADDS PS5 TO CART AND GOES TO CHECKOUT
     clickButton(addToCart)
     clickButton(checkOut)
     clickButton(continueWithoutAccount)
 
-    #FILLS OUT SHIPPING INFO
+    # FILLS OUT SHIPPING INFO
     clickButton(firstContinue)
     enterData(firstName, myFirstName)
     enterData(lastName, myLastName)
@@ -46,16 +53,16 @@ def order():
     enterData(address, myAddress)
     clickButton(confirmInfo)
 
-    #FILLS OUT PAYMENT
+    # FILLS OUT PAYMENT
     enterData(creditCardNum, myCreditCardNum)
     enterData(creditExpireMonth, myCreditExpireMonth)
     enterData(creditExpireYear, myCreditExpireYear)
     enterData(creditCVV, myCVV)
 
-
-    #ORDER
+    # ORDER
     clickButton(reviewOrder)
-    #clickButton(confirmOrder)
+    # clickButton(confirmOrder)
+
 
 def clickButton(xpath):
     try:
@@ -65,13 +72,15 @@ def clickButton(xpath):
         time.sleep(1)
         clickButton(xpath)
 
-def enterData(field,data):
+
+def enterData(field, data):
     try:
         driver.find_element_by_xpath(field).send_keys(data)
         pass
     except Exception:
         time.sleep(1)
-        enterData(field,data)
+        enterData(field, data)
+
 
 if __name__ == "__main__":
     driver = webdriver.Chrome("C:/Users/Alex/Downloads/chromedriver_win32/chromedriver.exe")
